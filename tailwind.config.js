775c43f84b8da7e5d644471d4ae2216da9fae834
Bill Nagbe <BillyNagbe@gmail.com>
2025-02-25 13:08:30 -0600

    sets up styles

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          "red": "var(--red)",
+          "green": "var(--green)",
+          "lite-blue": "var(--lite-blue)",
+          "drk-blue": "var(--drk-blue)",
+          "gray-blue": "var(--gray-blue)"
+        },
+        FontFace: {
+            "poppins": "var(--ff)"
+        }
+      },
+    },
+  };
